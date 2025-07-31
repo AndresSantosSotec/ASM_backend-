656877c7dcbd20b30637b6ddcf7664b054e755a2
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-07-31 11:28:00 -0600

    asdfasdfas

--- database/migrations/2025_06_25_160510_create_tb_programas_table.php
+++ database/migrations/2025_06_25_160510_create_tb_programas_table.php
@@ -6,32 +6,29 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('tb_programas', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('abreviatura', 50)->nullable();
-            $table->string('nombre_del_programa');
-            $table->integer('meses')->default(0);
-            $table->integer('area_comun')->default(0);
-            $table->integer('cursos_de_bba')->default(0);
-            $table->integer('area_de_especialidad')->default(0);
-            $table->integer('seminario_de_gerencia')->default(0);
-            $table->integer('capstone_project')->default(0);
-            $table->integer('escritura_de_casos')->default(0);
-            $table->integer('certificacion_internacional')->default(0);
-            $table->integer('total_cursos')->default(0);
-            $table->timestamp('fecha_creacion')->useCurrent();
-            $table->boolean('activo')->default(true);
-        });
+        // Sólo crea la tabla si no existe
+        if (! Schema::hasTable('tb_programas')) {
+            Schema::create('tb_programas', function (Blueprint $table) {
+                $table->id();
+                $table->string('abreviatura', 50)->nullable();
+                $table->string('nombre_del_programa');
+                $table->integer('meses')->default(0);
+                $table->integer('area_comun')->default(0);
+                $table->integer('cursos_de_bba')->default(0);
+                $table->integer('area_de_especialidad')->default(0);
+                $table->integer('seminario_de_gerencia')->default(0);
+                $table->integer('capstone_project')->default(0);
+                $table->integer('escritura_de_casos')->default(0);
+                $table->integer('certificacion_internacional')->default(0);
+                $table->integer('total_cursos')->default(0);
+                $table->timestamp('fecha_creacion')->useCurrent();
+                $table->boolean('activo')->default(true);
+            });
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('tb_programas');
