59547ff902b1f746f21b2c1150be2dd629f965d7
Pablo Santos <121809720+AndresSantosSotec@users.noreply.github.com>
2025-09-19 14:05:13 -0600

    asdf

--- config/session.php
+++ config/session.php
@@ -129,7 +129,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
     ),
 
     /*
@@ -197,7 +197,7 @@ return [
     |
     */
 
-    'same_site' => 'lax',
+    'same_site' => env('SESSION_SAME_SITE', 'none'),
 
     /*
     |--------------------------------------------------------------------------
